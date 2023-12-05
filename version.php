a5bc73da2106a4824ad04953a650c7be9f29a9e4
marcel <marcel@ghwalin.ch>
2023-12-05 20:34:00 +0100

    resolves #1, #2, #3, #4

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023111102;
+$plugin->version = 2023111301;
 $plugin->requires = 2015111000;
 $plugin->component = 'local_gradeassignments';
 $plugin->dependencies = array(
